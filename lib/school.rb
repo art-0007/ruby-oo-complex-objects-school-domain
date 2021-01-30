f5c655395db4663d7414cddb9be5f7025d292fef
Artur <info@aigroupusa.com>
2021-01-30 12:11:53 -0500

    commit tree 39eec393ad4a5e21dd825bcb020f262156b6b1e2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :school, :roster
+def initialize(school)
+    @school = school
+    @roster = Hash.new {|h,k| h[k]=[]}
+end
+
+
+def add_student(name, grade)
+@roster[grade] << name
+@roster
+end
+
+def grade(grade)
+@roster[grade]
+  end
+
+  def sort
+    return @roster.collect {|k, v| [k, v.sort]}.to_h
+    end
+
+end
